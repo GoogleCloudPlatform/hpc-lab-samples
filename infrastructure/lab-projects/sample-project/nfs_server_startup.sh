92ebb8019a7afbd4957b698172ba5cca90f3490e
Sujit Khasnis <skhas@google.com>
2021-09-01 18:33:48 -0500

    fix cross review #1

--- infrastructure/lab-projects/sample-project/nfs_server_startup.sh
+++ infrastructure/lab-projects/sample-project/nfs_server_startup.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
 yum -y install nfs-utils
+
 systemctl start nfs-server rpcbind
 systemctl enable nfs-server rpcbind
+
 mkdir -p "/home"
 mkdir -p "/tools"
+
 chmod 777 "/home" "/tools"
+
 echo '/home/ *(rw,sync,no_root_squash)' >> "/etc/exports"
 echo '/tools/ *(rw,sync,no_root_squash)' >> "/etc/exports"
+
 exportfs -r
